264c03e1baef761c825968aadde9ee0ee40ec563
Tirumala-Sai-Teja <80871045+Tirumala-Sai-Teja@users.noreply.github.com>
2021-05-04 20:17:41 +0530

    Update BestTimeToBuyandSellStock.py

--- BestTimeToBuyandSellStock.py
+++ BestTimeToBuyandSellStock.py
@@ -8,4 +8,20 @@ class Solution:
             elif (maxx-prices[i])>result:
                 result=maxx-prices[i]
         return result
+   #using dynamic programming top down
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         
+        l=prices
+        k=[0]*len(l)
+        m=[0]*len(l)
+        
+        m[-1]=k[-1]-l[-1]
+
+
+        for i in range(len(l)-2,-1,-1):
+            k[i]=max(l[i+1],k[i+1])
+            m[i]=k[i]-l[i]
+            
+    
+        return(max(m) if max(m)>0 else 0)
