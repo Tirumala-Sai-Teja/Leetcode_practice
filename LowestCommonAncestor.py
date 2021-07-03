240b826db4d3a743fd1a4b361e7ff18847340ca3
Tirumala-Sai-Teja <80871045+Tirumala-Sai-Teja@users.noreply.github.com>
2021-07-03 21:40:22 +0530

    Create LowestCommonAncestor.py

---
+++ LowestCommonAncestor.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root.val==p.val or root.val==q.val:
+            return root
+        l=self.lowestCommonAncestor(root.left,p,q)
+        r=self.lowestCommonAncestor(root.right,p,q)
+        if(l and r):
+            return root
+        return r if not l else l
