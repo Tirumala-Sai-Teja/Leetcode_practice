b8198684674b65886ff075f9cef80808c05d6560
Tirumala-Sai-Teja <80871045+Tirumala-Sai-Teja@users.noreply.github.com>
2021-07-12 22:41:39 +0530

    Update BFS.cpp

--- algorithms/BFS.cpp
+++ algorithms/BFS.cpp
@@ -5,16 +5,6 @@ otherwise we dont need to call dfs in a separate for loop*/
 #define MAXN 100005
 bool visited[MAXN];
 vector<int> adjList[MAXN];
-void dfs(int u){
-  if(visited[u]==true)
-    return;
-  visited[u]=true;
-  cout<<u<<" ";
-  for(int v: adjList[u]){
-    if(visited[v]==false)
-      dfs(v);
-  }
-}
 int main(){
   int n, m;
   cin>>n>>m;
@@ -26,14 +16,28 @@ int main(){
     adjList[b].pudh_back(a);
   }
   int root;
-  for(int i=0;i,=n;i++)
-    visited[i]=false;
   for(int i=1;i<=n;i++)
   {
-    if(visited[i]=true)
-      continue;
-    else:
-      root=i;
-    dfs(i);
+    if(visited[i]==true)
+        continue;
+    else
+        root=i;
+    queue<int>Q;
+    Q.push(root);
+    visited[root]=true;
+    while(!Q.empty())
+    {
+      int u=Q.front();
+      Q.pop();
+      for(int v: adjList[u])
+      {
+        if(visited[v]==false)
+        {
+          Q.push(v);
+          visited[v]=true;
+        }
+      }
+      cout<<u<<" ";
+    }
   }
 }
