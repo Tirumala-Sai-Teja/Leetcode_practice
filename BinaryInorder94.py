a52b4c731b03b87d384c38090a627a85f68a9251
Tirumala_Sai_Teja <80871045+Tirumala-Sai-Teja@users.noreply.github.com>
2022-05-11 22:24:09 +0530

    Update BinaryInorder94.py

--- BinaryInorder94.py
+++ BinaryInorder94.py
@@ -5,3 +5,16 @@ class Solution:
         res=[root.val]
         return self.inorderTraversal(root.left)+res+self.inorderTraversal(root.right)
     
+    
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        self.inorder(ans,root)
+        return ans
+    def inorder(self,ans,root):
+        if not root:
+            return 
+        self.inorder(ans,root.left)
+        ans.append(root.val)
+        self.inorder(ans,root.right)
+           
